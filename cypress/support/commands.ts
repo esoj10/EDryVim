df2020f9ca189f4513dfbc96fb0dca85245d1ab9
Jose Barrantes <jbarrantes@tekdatum.com>
2022-06-21 16:39:17 -0600

    Feat(struture): change the structure to typescript

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,9 +1,32 @@
-declare namespace Cypress {
-    interface Chainable<Subject> {
-        dataCy: any
-    }
-}
+import * as authorizationAPI from "./EP/authorization.command";
+import * as clientServiceAPI from "./API/clientService.command"
+import * as safetyServiceAPI from "./API/safetyService.command"
+import * as utilsAPI from "./API/utils.command"
+import cypress = require("cypress");
 
-Cypress.Commands.add('dataCy', (data: string) => {
-    return cy.get('[data-cy=' + data + ']')
-})
+//Cypress.Commands.add("",); 
+
+/*
+    Autorization
+*/
+Cypress.Commands.add("authorizationAPApi", authorizationAPI.authorization);
+
+/*
+    Client Service
+*/
+Cypress.Commands.add("getMessageAcceptanceReportAPI",
+  clientServiceAPI.getMessageAcceptanceReport);
+
+/*
+    Safety Service
+*/
+Cypress.Commands.add("sendNotificationsForNewModuleAvailableAPI",
+  safetyServiceAPI.sendNotificationsForNewModuleAvailable);
+
+
+/*
+    utils
+*/
+Cypress.Commands.add("bodyRequestAPI", utilsAPI.bodyRequest);
+Cypress.Commands.add("lstAPI", utilsAPI.lst);
+Cypress.Commands.add("recAPI", utilsAPI.rec);
